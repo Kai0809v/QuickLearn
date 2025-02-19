9af9ddc98c2deef8c14ae219f8aa5ca927b21c7c
Kai0809v <kai0809v@gmail.com>
2025-02-19 11:40:42 +0800

    遇到了编译器自身的bug，暂时搁置，具体是使用安卓虚拟机的时候会导致我的电脑黑屏重启，以前就有这个bug，最近概率变高了，我受不了了

--- app/src/main/java/com/example/learn/InfActivity.kt
+++ app/src/main/java/com/example/learn/InfActivity.kt
@@ -1,6 +1,11 @@
 package com.example.learn
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,10 +15,33 @@ class InfActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.infpage)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+//使用了View类，还是要声明的
+        val iButton1 :Button = findViewById(R.id.inf_b1)
+        val iButton2 :Button = findViewById(R.id.inf_b2)
+
+
+        val clickListener = View.OnClickListener { view ->
+            when (view.id) {
+                R.id.inf_b1 -> {
+                    // 处理按钮1的点击事件
+                    val url = "https://github.com/Kai0809v/QuickLearn" // 替换为你要跳转的网址
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+//                    if (intent.resolveActivity(packageManager) != null) {
+//                        startActivity(intent)
+//                    }
+                    startActivity(intent)
+                    println("Button 1 clicked")
+                }
+                R.id.inf_b2 -> {
+                    // 处理按钮2的点击事件
+                    Toast.makeText(this,"等小星星够多的时候再创建",Toast.LENGTH_SHORT).show()
+                    println("Button 2 clicked")
+                }
+            }
+        }
+
+        iButton1.setOnClickListener(clickListener)
+        iButton2.setOnClickListener(clickListener)
+
     }
 }
